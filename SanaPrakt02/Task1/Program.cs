da67f1eaa265de69f7db5be6fdc79a73daabf9cb
Eugune Biletskyi <biletskijzena@gmail.com>
2022-11-07 00:09:03 +0200

    Output 2 Hello world and created readline

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Hello World!");
+Console.WriteLine("Hello World!");
+Console.ReadLine();
